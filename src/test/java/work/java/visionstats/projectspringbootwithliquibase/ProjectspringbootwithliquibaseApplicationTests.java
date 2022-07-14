4b8c916a8a5ba44230471d5988e817ea2cd04d3e
jabbarDevOps <jabbar.devops@gmail.com>
2022-07-14 12:47:54 +0000

    ce projet est une mise en place des bonne pratique a savoir sur l'integration continue d'un systeme de gestion de base de donnees lors d'une colaboration dans un projet agile . l'outil utiliser est liquibase

---
+++ src/test/java/work/java/visionstats/projectspringbootwithliquibase/ProjectspringbootwithliquibaseApplicationTests.java
@@ -0,0 +1,13 @@
+package work.java.visionstats.projectspringbootwithliquibase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectspringbootwithliquibaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
